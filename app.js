22efe54a527c7ed9df1d402eebf8694d879bc673
Wasif Baliyan <w.baliyan@gmail.com>
2020-12-12 16:15:11 +0530

    refactor: fix some styling with clean code

--- app.js
+++ app.js
@@ -6,11 +6,10 @@ const url = "https://api.funtranslations.com/translate/minion.json";
 btn.addEventListener("click", () => {
   let value = textArea.value;
   const newUrl = url + "?text=" + value;
-  textArea.value = "";
+
   fetch(newUrl)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
       translation.innerHTML = data.contents.translated;
     })
     .catch((error) => alert("something went wrong", error));
